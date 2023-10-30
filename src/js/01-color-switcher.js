19e842f6fe665d51e353de4d57a621e73b0414dd
andrewbal93 <131342609+andrewbal93@users.noreply.github.com>
2023-10-30 21:12:36 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.body;
+let colorInterval;
 
+stopBtn.disabled = true;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16).padStart(6, 0)}`;
+}
+
+startBtn.addEventListener('click', () => {
+    colorInterval = setInterval(() => bodyEl.style.backgroundColor = getRandomHexColor(), 1000)
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+})
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(colorInterval);
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+})
